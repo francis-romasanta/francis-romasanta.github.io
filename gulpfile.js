1eaf1949470cfbc6df362e667c3db4df155be82b
Francis <francisromasanta12@gmail.com>
2024-04-10 14:46:09 +0800

    add local dev and resume

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,9 @@ var plumber = require('gulp-plumber');
 const uglify = require('gulp-uglify');
 const sass = require('gulp-sass');
 const wait = require('gulp-wait');
-const babel = require('gulp-babel');;
+const babel = require('gulp-babel');
 const rename = require('gulp-rename');
+const browserSync = require('browser-sync').create(); // Import browser-sync
 
 gulp.task('scripts', function() {
     return gulp.src('./js/scripts.js')
@@ -23,17 +24,41 @@ gulp.task('scripts', function() {
             }
         }))
         .pipe(rename({extname: '.min.js'}))
-        .pipe(gulp.dest('./js'));
+        .pipe(gulp.dest('./js'))
+        .pipe(browserSync.stream()); // Stream changes to browser-sync
 });
 
 gulp.task('styles', function () {
     return gulp.src('./scss/styles.scss')
         .pipe(wait(250))
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.stream()); // Stream changes to browser-sync
 });
 
 gulp.task('watch', function() {
     gulp.watch('./js/scripts.js', gulp.series('scripts'));
     gulp.watch('./scss/styles.scss', gulp.series('styles'));
 });
+
+// Task to start the development server
+gulp.task('serve', function(done) {
+    debugger;
+    browserSync.init({
+        server: './', // Serve files from the current directory
+        port: 8080 // Specify the port here
+    }, function(err, bs) {
+        if (err) {
+            console.error('Error starting server:', err);
+        } else {
+            console.log('Server running at:', bs.options.get('urls').get('local'));
+        }
+        done();
+    });
+
+    // Watch for changes in HTML files and reload the browser
+    gulp.watch('./*.html').on('change', browserSync.reload);
+});
+
+// Default task to run the server and watch tasks together
+gulp.task('default', gulp.parallel('serve', 'watch'));
